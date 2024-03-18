05a98dfd4971518045cb2ad5e73f673ee2cf604c
Guillermo Guzmán <Billy Idle>
2024-03-18 10:08:57 -0500

    Fixed I don't know

--- src/main/java/com/github/agify/prediction/PredictionController.java
+++ src/main/java/com/github/agify/prediction/PredictionController.java
@@ -3,18 +3,20 @@ package com.github.agify.prediction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
 @RestController
+@RequestMapping("/api/agify")
 @RequiredArgsConstructor
 class PredictionController {
 
     private final PredictionService predictionService;
 
-    @GetMapping
+    @GetMapping()
     Prediction getPrediction(@RequestParam String name) {
         return predictionService.query(name);
     }
